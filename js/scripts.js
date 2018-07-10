42ef7c169dea266d99c723086a07b0c2c0f067cd
Maurice <bitmogram@gmail.com>
2018-07-09 18:03:27 -0700

    First push after accidentally deleting everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-//DECLARE A VARIABLE
-var firstName;
+// //DECLARE A VARIABLE
+// var firstName;
+//
+// //DEFINE YOUR VARIABLE
+// firstName ="Maurice";
+//
+// //DEFINE AND DECLARE YOUR VARIABLE AT THE SAME TIME
+// var lastName = "Graham"
+//
+// //MAKE YOUR VAR NAMES AS INTUITIVE AS POSSIBLE
+// //ADD COMMENTS THAT DESCRIBE WHAT YOUR VARIABLES DO
+//
+// console.log(firstName, lastName)
+//
+// firstName = "Batman";
+// console.log(firstName);
+// //all functions should be terminated with a semi colon.
+//
+// //THIS IS A SINGLE LINE COMMENT
+//
+// /*
+// EXAMPLE OF MULTILINE CODE
+// firstName = "Superman";
+// console.log(firstName);
+// */
+//
+// var getTweetDataAsync; //Labling :) This is the variable that gets the tweets from twitter.
+//
+// {
+//   //CURLY BRACKETS INDICATE THAT YOU HAVE CREATED A NEW SCOPE.
+//   //curly brackets add an indent in your js.
+//   var myName;
+// }
 
-//DEFINE YOUR VARIABLE
-firstName ="Maurice";
-
-//DEFINE AND DECLARE YOUR VARIABLE AT THE SAME TIME
-var lastName = "Graham"
-
-//MAKE YOUR VAR NAMES AS INTUITIVE AS POSSIBLE
-//ADD COMMENTS THAT DESCRIBE WHAT YOUR VARIABLES DO
-
-console.log(firstName, lastName)
-
-firstName = "Batman";
-console.log(firstName);
-//all functions should be terminated with a semi colon.
-
-//THIS IS A SINGLE LINE COMMENT
-
-/*
-EXAMPLE OF MULTILINE CODE
-firstName = "Superman";
-console.log(firstName);
-*/
-
-var getTweetDataAsync; //Labling :) This is the variable that gets the tweets from twitter.
-
-{
-  //CURLY BRACKETS INDICATE THAT YOU HAVE CREATED A NEW SCOPE.
-  //curly brackets add an indent in your js.
-  var myName;
-}
-
-var myGoals = "Testing terminal"
+var myGoals = "Testing terminal";
+console.log(myGoals);
