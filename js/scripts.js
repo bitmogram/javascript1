c1d17e859a1169fe9b3e53b3c7fd4cdc0cff3d3b
Maurice <bitmogram@gmail.com>
2018-07-11 18:34:44 -0700

    beginning of first in class assignment

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,9 @@ switch (favColor.toLowerCase()) {
 }
 }
 //Beginning of day 4 notes!
+
+let myArray = []; //Most basic form of myArray
+let favoriteFoods = ["pizza", "hamburgers", "french fries"]; //study how to count the length of arrays.
+let allTheThings = ["string", 23, ["stuff"], {}, false]; //This is an array. Most of the data you will get when dealing with data are usually inside of an array.
+
+console.log(favoriteFoods[1]); //We are targeting the 2nd member of the array which number is one because we start counting from zero.
