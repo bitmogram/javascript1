ab7695f494e14031921fb2fdd9cdf1ef2412729b
Maurice <bitmogram@gmail.com>
2018-07-11 18:20:09 -0700

    beginning of class

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Take Home Assignment. Done in Class
+
+let favColor = prompt('what is your name?');
+let favColor = prompt("Tell me your favorite color", "Please use");
+
+if(name && name !== '') {
+
+switch (favColor.toLowerCase()) {
+  case 'blue':
+    alert('Hey ${name}, $favColor} is the best color');
+    break;
+    //...
+  default:
+    alert('You did not give me a valid color!');
+} else {
+  alert('You suck!');
+}
+}
+//Beginning of day 4 notes!
