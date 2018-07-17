3ed43ec29b3996021375bdbd60c9f5d5a18048e1
Maurice <bitmogram@gmail.com>
2018-07-16 19:03:00 -0700

    push during break in class

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+// for (let i = 0; i < 3; i++) {
+//   console.log('hi!');
+// }
+//
+// for (let i = 0; i < 11; i++) {
+//   console.log('The number is ${i * 10}');
+// }
+
+//
+// const colors = ['blue', 'purple', 'grey', 'red', 'aqua', 'green'];
+
+colsole.log(color[0]);
+console.log(color[1]);
+console.log(color[2]);
+for( let index = 0; index < color.length; index++) {
+  console.log(colors[index]);
+}
+
+//FOR LOOP
+//for ( let index = 0; index < colors.length; index++ ) {
+//    console.log(colors[index]);
+//      if (index === 1) break;  //this line breaks out of the loop if condition is met. (if you need to get out of a loop)
+
+//}
+
+//you can use break and continue to end or continue loops.
+//for loops are more common and used more often.
+//the while loop reads a bit more closely to english.
+
+//while loop
+let counter =0;
+while (counter < 5) {
+  console.log('i like food');
+  counter++;
+  console.log('after ${counter}');
+}
+
+//for each example
+const captains = [ 'kirk', 'picard', 'sisco', 'janeway','lorca' ];
+captains.forEach( captain => {
+  console.log(captain);
+})
+
+// this is called fat arrow =>
