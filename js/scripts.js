e38ae707547880f054a86edd61086f4dd72e4382
Maurice <bitmogram@gmail.com>
2018-07-18 18:33:19 -0700

    In class commit on second half of day5

--- js/scripts.js
+++ js/scripts.js
@@ -72,3 +72,44 @@ string => { //parenthesis around the word string are optional}
 //IMPLICIT RETURN.. both these examples are the same.
 string => (string.toUpperCase());
 STRING => string.toUpperCase;
+
+
+//IN CLASS DAY 5 PART TWO.
+
+//Try it out #1
+// On your day5 branch, write a function that calculates a dog's age. It should:
+//
+// Accept 1 argument (age of the dog in years).
+// Return the age of the dog using the conversion rate of 1 year to 7 "dog" years.
+// Alert the answer.
+// Make sure you're checking for edge-cases!
+//
+// Now, write a function that tells you what a "lifetime supply" will be for any product. It should:
+//
+// Accept 2 arguments (age of person now, amount of product per day).
+// Calculate the supply needed for the rest of the user's life, based on a constant max age of 80.
+// Alert the answer in this format: "You will need x to last you until you're y.
+// BONUS: write it to accept a dynamic max age.
+
+function calcLifetimeSupply(currentAge, dailyUse, maxAge)
+{
+  if(isNaN(currentAge) && isNaN(dailyUse)) {
+    alert("Please give me a number!");
+    let currentAgeSecondTry = prompt("Whats your age?");
+    let dailyUseSecondTry = prompt("How many per year?");
+    calcLifetimeSupply(currentAgeSecondTry, maxAgeSecondTry);
+  } else {
+    if(currentAge < maxAge) {
+    let supply = (maxAge - currentAge) * (dailyUse * 365;
+  alert('you will need ${supply} to last until youre
+  ${endOfLife}');
+}
+
+
+//NOT ALL FUNCTIONS ARE METHODS, BUT ALL METHODS ARE FUNCTIONS.
+
+//log is a function.
+//console is the object - log. is the method.
+
+//METHODS
+console.log(232323); //log was written to consume arguments.
